4881177f11a3f9d53eeeb4d91a21b4a61f2ca115
Batoshu <bartosz@staniek.net>
2025-01-19 18:12:22 +0100

    Create Message class

--- public/scripts.js
+++ public/scripts.js
@@ -27,6 +27,10 @@
         enableLoginButton() {
             const button = document.querySelector('#login-button');
             button.removeAttribute('disabled');
+        },
+        addMessage(message) {
+            const box = document.querySelector('#chat-window');
+            box.insertBefore(message.element, box.firstChild);
         }
     };
 
@@ -69,6 +73,43 @@
         }
     }
 
+    /* Fetched message */
+    class Message {
+        static messages = new Set();
+
+        static async fetchMessages(limit = 50, before = undefined) {
+            // TODO: Implement message fetcher
+        }
+
+        constructor(data) {
+            this.uuid = data.uuid;
+            this.userUuid = data.userUuid;
+            this.author = null;
+            this.message = data.message;
+            this.timestamp = new Date(data.timestamp);
+
+            this.element = document.createElement('li');
+            Message.messages.add(this);
+        }
+
+        get time() {
+            return this.timestamp.toLocaleString();
+        }
+
+        async render() {
+            const element = this.element;
+            if (!this.author)
+                this.author = User.get(this.userUuid);
+
+            element.innerHTML = `<span class="username">
+                    ${this.author.nickname ?? 'Unknown Author'} 
+                    <span class="datetime">${this.time}</span>
+                </span>
+                <span class="message">${this.message}</span>`;
+
+            return this.element;
+        }
+    }
 
     window.ui = ui;
 })().catch(console.error);
