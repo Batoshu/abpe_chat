1124547c063e0ce716903d8fedd774aa69c74913
Batoshu <bartosz@staniek.net>
2025-01-19 18:11:49 +0100

    Create User class

--- public/scripts.js
+++ public/scripts.js
@@ -30,5 +30,45 @@
         }
     };
 
+    /* Fetched user */
+    class User {
+        static users = new Map();
+        static async get(userUuid) {
+            if (this.users.has(userUuid))
+                return this.users.get(userUuid);
+            return null;
+            // TODO: Implement user data fetcher from server when not found in cache
+        }
+
+        constructor(data) {
+            if (User.users.has(data.uuid)) return;
+
+            this.uuid = data.uuid;
+            this.nickname = data.nickname;
+            this.ip = data.ip;
+            this.status = data.status;
+
+            this.element = document.createElement('li');
+            User.users.set(this.uuid, this);
+        }
+
+        updateStatus(status) {
+            this.status = status;
+            this.render().catch(console.error);
+        }
+
+        async render() {
+            const element = this.element;
+
+            element.textContent = this.nickname;
+            if (this.status === 'online')
+                element.style.color = 'green';
+            else
+                element.style.color = 'red';
+            return element;
+        }
+    }
+
+
     window.ui = ui;
 })().catch(console.error);
