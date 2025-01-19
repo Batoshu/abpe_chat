a4c6b8290a45ef136712eb110c76f7b02c5e4f1d
Batoshu <bartosz@staniek.net>
2025-01-19 17:36:24 +0100

    Add function to show alert box

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,26 @@
 (async function IIFE() {
-    console.log("IIFE executed");
+    /* UI manager */
+    const ui = {
+        get body() {return document.body},
+        alert(message, type = 'error', persistent = false) {
+            const alert = document.createElement('div');
+            alert.classList.add('notification-box');
+            alert.classList.add(type);
+            alert.innerHTML = message;
+
+            ui.body.append(alert);
+
+            let timeout;
+            let remove = () => {
+                clearTimeout(timeout);
+                ui.body.removeChild(alert);
+            }
+
+            alert.onclick = remove;
+            if (!persistent)
+                timeout = setTimeout(remove, 6000);
+        }
+    };
+
+    window.ui = ui;
 })().catch(console.error);
